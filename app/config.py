093a932609cbe7b7c0693019f9dc1da08bc40a39
Korotynski Alexey <korotynski.alexey@gmail.com>
2019-03-31 23:54:29 +0300

    update password

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class Configuration:
     DEBUG = True
-    CELERY_BROKER_URL = 'amqp://admin:esmwu&astr@localhost:5672/rabbit@Y50-70'
+    CELERY_BROKER_URL = 'amqp://admin:esmwu&astr@localhost:5672/'
