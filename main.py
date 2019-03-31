57ebfa6c6f93700bec3dda5b494a70d6163bbd87
Korotynski Alexey <korotynski.alexey@gmail.com>
2019-03-31 18:59:20 +0300

    add celery

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from app.application import APP
 from app.core.database import DB
-from app.core.scraper import get_categories_structure
+from app.core.parser import get_categories_structure
 from app import view
 
 if __name__ == '__main__':
     if not DB.list_collection_names():
         get_categories_structure()
+        DB.products.create_index('key', unique=True)
     APP.run()
